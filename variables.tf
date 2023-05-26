b7bc62e5db49f9674743ba1ebc579b44ffbd396f
ivycomtech <134664057+ivycomtech@users.noreply.github.com>
2023-05-26 11:36:27 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+## Azure API Key
+#############################################################################################
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
+## Resource Group 변수
+#############################################################################################
+variable "rg" {
+  description = "Resource Group 변수 선언 및 데이터 입력"
+  type = object({
+    rg_name      = string
+    rg_location  = string
+    tags         = map(string)
+  })
+  default = {
+    rg_name      = "Naver_resource_group"
+    rg_location  = "koreacentral"
+    tags         = { source : "terraform" }
+  }
+}
