813d0eeb10b0429a1fe27a67037083277b136928
too3245 <too3245@ivycomtech.com>
2023-06-01 13:03:01 +0900

    webhook - send Test v.0.988

--- variables.tf
+++ variables.tf
@@ -17,6 +17,6 @@ variable "rg" {
   default = {
     rg_name      = "Naver_resource_group"
     rg_location  = "koreacentral"
-    tags         = { source : "terraform_12" }
+    tags         = { source : "terraform_13" }
   }
 }
