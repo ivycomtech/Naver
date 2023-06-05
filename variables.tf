e2f99792d9edd53453b7a6a481e31fa905490a1c
남 용우 <nyw@ivycomtech.kr>
2023-06-05 16:04:05 +0900

    수정

--- variables.tf
+++ variables.tf
@@ -17,6 +17,6 @@ variable "rg" {
   default = {
     rg_name      = "Naver_resource_group"
     rg_location  = "koreacentral"
-    tags         = { source : "terraform_37" }
+    tags         = { source : "terraform_38" }
   }
 }
